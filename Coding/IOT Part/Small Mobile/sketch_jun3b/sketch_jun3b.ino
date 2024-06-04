e646a537304f74a986b2b98360700bf5458d6c11
Junior <jagadeeswarreddy4444@gmail.com>
2024-06-04 22:09:39 +0530

    updates

--- Coding/IOT Part/Small Mobile/sketch_jun3b/sketch_jun3b.ino
+++ Coding/IOT Part/Small Mobile/sketch_jun3b/sketch_jun3b.ino
@@ -1,30 +1,38 @@
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <DHT_U.h>
+#include <SoftwareSerial.h>
 
 #define POWER_PIN  8
 #define SIGNAL_PIN A4
 #define sensorPower 7
 #define sensorPin A0
-#define DHTPIN 2
-
+#define DHTPIN 4
 #define DHTTYPE    DHT11     // DHT 11
+
 DHT_Unified dht(DHTPIN, DHTTYPE);
 uint32_t delayMS;
-
 int value = 0; // variable to store the sensor value
+
+SoftwareSerial mySerial(3, 2); // SIM800L Tx & Rx connected to Arduino pins #3 & #2
 
 void setup() {
   Serial.begin(9600);
   pinMode(POWER_PIN, OUTPUT);   // configure D7 pin as an OUTPUT
   digitalWrite(POWER_PIN, LOW); // turn the sensor OFF
-  
   pinMode(sensorPower, OUTPUT);
   digitalWrite(sensorPower, LOW); // Initially keep the sensor OFF
-  
   dht.begin();
   sensor_t sensor;
   delayMS = sensor.min_delay / 1000;
+
+  mySerial.begin(9600);
+  Serial.println("Initializing..."); 
+  delay(1000);
+  mySerial.println("AT"); // Handshake test, should return "OK" on success
+  updateSerial();
+  mySerial.println("AT+CMGF=1"); // Configuring TEXT mode
+  updateSerial();
 }
 
 void loop() {
@@ -54,5 +62,30 @@ void loop() {
   Serial.print(event.relative_humidity);
   Serial.println(F("%"));
 
+  // Sending SMS
+  mySerial.println("AT+CMGS=\"+919553584470\""); // Change ZZ with the country code and xxxxxxxxxxx with the phone number to send an SMS to
+  updateSerial();
+  mySerial.print("Soil Moisture: ");
+  mySerial.println(moisture);
+  mySerial.print("Water Sensor value: ");
+  mySerial.println(value);
+  mySerial.print("Temperature: ");
+  mySerial.print(event.temperature);
+  mySerial.println("°C");
+  mySerial.print("Humidity: ");
+  mySerial.print(event.relative_humidity);
+  mySerial.println("%");
+  mySerial.write(26); // Send the CTRL+Z character to terminate the SMS
+
   delay(10000);
+}
+
+void updateSerial() {
+  delay(500);
+  while (Serial.available()) {
+    mySerial.write(Serial.read()); // Forward data from Serial to Software Serial Port
+  }
+  while (mySerial.available()) {
+    Serial.write(mySerial.read()); // Forward data from Software Serial to Serial Port
+  }
 }
